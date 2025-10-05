5ace4122757369d590deb9d4b01ca0835983d350
dennstack <dennis@schilakowski.de>
2025-10-05 22:14:02 +0200

    add database connection logic and environment variable loading

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load(".env")
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, dbname)
+	db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+	log.Println("Successfully connected to the database")
+
 	StartApiServer()
 }
